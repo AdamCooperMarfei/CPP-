3772b09cdc28a7fb01a0300821b2b94b77790bf2
adamcooper <adamcooper@adamcooperdeMacBook-Air.local>
2023-04-09 10:58:00 +0800

    reverseLinkedList

---
+++ Leetcode/线性表/单链表/reverseLinkedList.cpp
@@ -0,0 +1,26 @@
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(nullptr){}
+};
+class Solution{
+public:
+    ListNode *reverseLinkedList(ListNode *head, int m, int n){
+        ListNode dummy(-1);
+        dummy.next = head;
+        ListNode *prev = &dummy;
+        for(int i = 0;i < m - 1; ++i){
+            prev = prev->next;
+        }
+        ListNode* const head2 = prev;
+        prev = head2->next;
+        ListNode * cur = prev->next;
+        for(int i = m; i < n; ++i){
+            prev->next = cur->next;
+            cur->next = head2->next;
+            head2->next = cur;
+            cur = prev->next;
+        }
+        return dummy.next;
+    }
+};
